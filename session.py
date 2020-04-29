bc2ec658b74870f81f1080c81e10289e4251ecaa
rasha08 <rasha08@gmail.com>
2020-04-29 07:11:18 +0200

    fixed consent error

--- session.py
+++ session.py
@@ -14,7 +14,7 @@ def user_asked_to_consent_for_project(project_path):
 
 
 def add_project_to_asked_to_consent(project_path):
-    if user_asked_to_consent_for_current_project(project_path):
+    if user_asked_to_consent_for_project(project_path):
         return
 
     SESSION_STORAGE["asked_to_consent"].append(project_path)
